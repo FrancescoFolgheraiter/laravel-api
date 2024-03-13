ce4a9acf9090aedf86f2a5ab53d6874b86c00310
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-03-13 15:06:36 +0100

    edit api route and projectcontroller api

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+// Models
+use App\Models\Project;
+
+// Helpers per slug
+use Illuminate\Support\Str;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+        //tramite questa funzione ritorno il file json con al struttura delineata nelle []
+        return response()->json([
+            'code'=> 200,
+            'message'=>'ok',
+            'data'=> [
+                'projects'=> $projects
+            ]
+        ]);
+    }
+    //---------------------------------------------------------------
+    public function show(string $slug)
+    {
+        $project = Project::where('slug', $slug)->firstOrFail();
+
+        return view('admin.projects.show', compact('project'));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+//importazione controller API Project
+use App\Http\Controllers\Api\ProjectController as ApiProjectController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::name('api.')->group(function() {
+    
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+    //mia api per restituire json
+    Route::resource('projects', ApiProjectController::class)->only([
+        'index',
+        'show'
+    ]);
 });
